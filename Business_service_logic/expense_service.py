e82019c8c5b3bef8bb7726c125ea7d64200e7a49
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-10-22 01:21:27 -0700

    Add files via upload

---
+++ Business_service_logic/expense_service.py
@@ -0,0 +1,12 @@
+from config.db import db
+from datetime import datetime
+
+def add_expense(user_id, amount, category, expense_tyoe):
+    """add a new expense under a single category"""
+
+    # Rule 8
+
+
+def change_expense_category(user_id, expense_id, new_category):
+    """enforce rule 9 here"""
+    return

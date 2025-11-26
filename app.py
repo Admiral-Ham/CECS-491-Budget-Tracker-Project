01aa20a914dfc30a6a13ac7e6f359b37d9776f9b
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-11-26 00:51:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from routes.category_routes import category_bp
 from routes.expenses_routes import expense_bp
 
 def start_app():
-app = Flask(__name__) # initialized the flask application for backend
+    app = Flask(__name__) # initialized the flask application for backend
 
 # Register blueprints
 app.register_blueprint(user_bp, category_bp, expense_bp, budget_bp, report_bp)

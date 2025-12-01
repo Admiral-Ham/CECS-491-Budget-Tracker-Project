194d43ab8edf20ddb84b6a774036dfdb1027563a
Admiral-Ham <ai.does.math152@gmail.com>
2025-11-30 20:08:34 -0800

    Commented out file

--- utils/ObjectId_helper.py
+++ utils/ObjectId_helper.py
@@ -1,15 +1,16 @@
-"""from bson import ObjectId
+"""
+from bson import ObjectId
 from flask import jsonify
 
 def to_objectid(id_str):
-  """Converts string to objectID or returns None"""
+  #Converts string to objectID or returns None
   try:
     return ObjectId(id_str)
   except Exception:
     return None
 
 def obj_to_json(doc):
-  """Convert _id fields and nested ObjectId to strings for JSON response."""
+  #Convert _id fields and nested ObjectId to strings for JSON response.
   if not doc:
     return None
   # conversion 
@@ -18,7 +19,8 @@ def obj_to_json(doc):
   # if user_id exists, convert
   if "user_id" in doc and isinstance(doc["user_id"], ObjectId):
     doc["user_id] = str(doc["user_id"])
-  return doc"""
+  return doc
 
   # work in progress
   
+"""

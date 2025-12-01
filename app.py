1523ab7019bc83e6ed017c121404eb5b4c00560e
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-12-01 14:18:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, request, jsonify
-# blueprints from routes folder, contains API endpoints
-# blueprints are used here to register the apps url prefix to flask in order to run the APIs.
+from flask import Flask
 from routes.auth_routes import auth_bp
 from routes.budget_routes import budget_bp
-#from routes.expense_routes import expense_bp
-#from routes.
-app = Flask(__name__)
+from routes.expense_routes import expense_bp
+from routes.category_routes import category_bp
+from routes.report_routes import report_analytics_bp
 
-# Registering blueprints 
-app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(budget_bp, url_prefix="/budget")
+def create_app():
+    app = Flask(__name__)
+
+    # contains api endpoints to establish connection to front end inside Flask upon initialization
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(budget_bp)
+    app.register_blueprint(expense_bp)
+    app.register_blueprint(category_bp)
+    app.register_blueprint(report_analytics_bp)
+
+
+if __name__ == "__main__":
+    create_app().run(debug=True)
+
 
 #testing login api
 """@app.post("/login")
@@ -22,7 +31,4 @@ def login():
         return jsonify({"success": True, "message": "Login Ok"})
     else:
         return jsonify({"success": False, "message": "Login Failed"})"""
-
-if __name__ == "__main__":
-    app.run(debug=True)
 

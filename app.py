37dba9858909d37969141d5a3a8e0cb625366789
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-11-30 21:02:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
-
+# blueprints from routes folder, contains API endpoints
+# blueprints are used here to register the apps url prefix to flask in order to run the APIs.
+from routes.auth_routes import auth_bp
+from routes.budget_routes import budget_bp
+#from routes.expense_routes import expense_bp
+#from routes.
 app = Flask(__name__)
 
-@app.post("/login")
+# Registering blueprints 
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(budget_bp, url_prefix="/budget")
+
+#testing login api
+"""@app.post("/login")
 def login():
     data = request.json()
     username = data.get("username")
@@ -11,7 +21,7 @@ def login():
     if username == "TechnicSolutions" and password == "1234":
         return jsonify({"success": True, "message": "Login Ok"})
     else:
-        return jsonify({"success": False, "message": "Login Failed"})
+        return jsonify({"success": False, "message": "Login Failed"})"""
 
 if __name__ == "__main__":
     app.run(debug=True)

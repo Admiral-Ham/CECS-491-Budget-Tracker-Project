8c568c1083153b0661cc359acbd28e10e2989f28
anthonytheng <theng.anthonyy@gmail.com>
2025-12-01 16:08:25 -0800

    Main has forget password, some changes to landing page

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from kivy.uix.screenmanager import ScreenManager
 
 from pages.login_page import LoginScreen
 from pages.landing import LandingScreen
-from pages.settings import SettingsScreen
+
+import webbrowser
+
+import re
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.textfield import MDTextField
 
 class MyApp(MDApp):
     def build(self):
         self.title = "DEMO FOR BUDGET APP"
         self.theme_cls.primary_palette = "Teal"
         self.theme_cls.theme_style = "Dark"
+        self.forgot_dialog = None
+        self.forgot_email_field = None
 
         sm = ScreenManager()
         sm.add_widget(LoginScreen(name="login"))
         sm.add_widget(LandingScreen(name="landing"))
-        sm.add_widget(SettingsScreen(name="settings"))
         sm.current = "login"
         return sm
-    
-    def go_to(self, screen_name):
-        self.root.current = screen_name
+
+        
         
 
     def check_login(self, username, password):
@@ -34,7 +40,84 @@ class MyApp(MDApp):
             self.root.current = "landing"
         else:
             login.ids.status.text = "Login Failed"
-            login.ids.password.text = ""  
+            login.ids.password.text = ""
+            
+    #These next two functions will probably be deleted but here for example sakes
+    def show_forgot_password(self):
+        if not self.forgot_dialog:
+            self.forgot_email_field = MDTextField(
+                hint_text="Enter your email",
+                helper_text="Instructions for email reset sent or something",
+                helper_text_mode="on_focus",
+                size_hint_y=None,
+                height="56dp",
+                
+            )
+            
+            self.forgot_dialog = MDDialog(
+                title="Forgot password",
+                type="custom",
+                content_cls=self.forgot_email_field,
+                buttons=[
+                    MDFlatButton(
+                        text="Cancel",
+                        on_release=lambda *args: self.forgot_dialog.dismiss()
+                    ),
+                    MDFlatButton(
+                        text="Send",
+                        on_release=lambda *args: self.send_reset_email()
+                    ),
+                ],
+            )
+        self.forgot_email_field.text = ""
+        self.forgot_dialog.open()
+        
+    def send_reset_email(self):
+        email = (self.forgot_email_field.text or "").strip()
+        login = self.root.get_screen("login")
+        status = login.ids.status
+        
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            status.text = "Please enter a valid email."
+            return
+        
+        #Backend goes here but for now funny texxt xd
+        status.text = "Work in Progress"
+        self.forgot_dialog.dismiss()
+            
+    # --- Functions for Settings ---
+    def open_learn_page(self):
+        webbrowser.open("https://consumer.gov/your-money/making-budget")
+    def open_privacy_page(self):
+        pass
+    def open_about_page(self):
+        pass
+    
+    # testing connection for login page
+        """response = requests.post(
+            "http://127.0.0.1:5000/login",
+            json={"email": username,
+                  "password": password
+            }
+        )
+
+        try:
+            response = requests.post(
+                "http://127.0.0.1:5000/login",
+                json={"username": username, "password": password}
+            )
+            data = response.json()
+
+            if data["success"]:
+                login.ids.status.text = data["message"]
+                self.root.current = "landing"
+            else:
+                login.ids.status.text = "Login Failed"
+                login.ids.password.text = ""
+
+        except Exception as e:
+            login.ids.status.text = "Server Error"
+            print("Error:", e)"""
     
 
 if __name__ == "__main__":
--- pages/landing.py
+++ pages/landing.py
@@ -2,7 +2,64 @@ from pathlib import Path
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 
+from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
+from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.scrollview import MDScrollView
+
 Builder.load_file(str(Path(__file__).resolve().parents[1] / "ui" / "landing.kv"))
 
+light_theme = ["white", "black", "Light"]
+dark_theme = ["black", "white", "Dark"]
+theme = dark_theme
+
+data = [1250, 650, 375, 150, 75]
+labels = ["Rent/House", "Loans & Debts", "Bills & Utilities", "Groceries", "Entertainment"]
+
+def pie_chart():
+    fig = plt.figure(facecolor=theme[0])
+    ax = fig.add_subplot()
+    ax.pie(data, startangle=90, wedgeprops=dict(width=0.3, edgecolor="w"))
+
+    center = plt.Circle((0, 0), 0.7, fc=theme[0])
+    plt.text(0, 0, f"Expenses\n -${sum(data)}", ha="center", va="center", fontsize=20, color=theme[1])
+    plt.gca().add_artist(center)
+    return fig
+
 class LandingScreen(MDScreen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.theme_cls.theme_style = theme[2]
+        
+        bottom_nav = MDBottomNavigation()
+
+        home_tab = MDBottomNavigationItem(name="home", text="Home", icon="home")
+        home_layout = MDBoxLayout(orientation="vertical", padding="5dp")
+        bar_layout = MDBoxLayout(orientation="vertical", adaptive_height=True, padding="5dp", spacing="2.5dp")
+        scroll = MDScrollView()
+        
+        for each in labels:
+            bar_layout.add_widget(
+                MDRaisedButton(
+                    text=f"{each}: -${data[labels.index(each)]}\n {int(data[labels.index(each)] / sum(data) * 100)}%",
+                    halign="left", 
+                    size_hint=(1, None), 
+                    pos_hint={"center_x": 0.5}))
+        
+        home_layout.add_widget(FigureCanvasKivyAgg(pie_chart()))
+        scroll.add_widget(bar_layout)
+        home_layout.add_widget(scroll)
+        home_tab.add_widget(home_layout)
+        bottom_nav.add_widget(home_tab)
+        
+        budget_tab = MDBottomNavigationItem(name="budget", text="Budget", icon="currency-usd")
+        bottom_nav.add_widget(budget_tab)
+        
+        calendar_tab = MDBottomNavigationItem(name="calendar", text="Calendar", icon="calendar")
+        bottom_nav.add_widget(calendar_tab)
+        
+        self.add_widget(bottom_nav)
+
+    

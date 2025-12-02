75350700e2163645f5ac21e209da160e6f3a10b8
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-12-01 16:38:44 -0800

    transaction_service

---
+++ Business_service_logic/transaction_service.py
@@ -0,0 +1,18 @@
+from config.db import db
+from datetime import datetime
+
+def add_transaction(budget_id):
+    
+    """add a new expense under a single category"""
+
+    # Rule 8
+
+
+def delete_transaction(budget_id):
+
+
+# call user collection because its embedded 
+"""one budget, to make categories easier, category is unique to budget
+call budget, search field for specific names category, rule in categories. """
+
+

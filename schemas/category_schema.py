e2e9ed655892bee85558e7b05ca8de515c135248
Admiral-Ham <ai.does.math152@gmail.com>
2025-12-04 18:33:25 -0800

    Updated Categories schema

--- schemas/category_schema.py
+++ schemas/category_schema.py
@@ -6,7 +6,8 @@ class Category(BaseModel):
     #id: Optional[str] = Field(default=None, alias="_id")
     #user_id: str 
     name: str
-    #type: str = Field(default="Need")
+    limit: float = Field(default= 0.0)
+    spent: float = Field(default= 0.0)
 
     model_config = {
         "populate_by_name": True,

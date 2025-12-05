b7ac5333981fc82cf0fa7016b4177e9cebc647b6
Admiral-Ham <ai.does.math152@gmail.com>
2025-12-04 20:30:49 -0800

    Fixed query for update_budget

--- data/budget_db.py
+++ data/budget_db.py
@@ -1,8 +1,65 @@
-class BudgetModel:
+from data.db import db
+from datetime import datetime
+
+class BudgetModel():
+
   @staticmethod
-  def find_by_user_id(user_id):
+  def find_by_user_id(user_id: str):
+    """
+    Docstring for find_by_user_id
+    
+    :param user_id: Description
+    :type user_id: str
+    """
     return db.budgets.find_one({"user_id": user_id})
 
   @staticmethod
-  def update_budget(budget_id, update_data):
-    return db.budgets.update_one({"_id": budget_id}, {"": update_data})
+  def create_budget(user_id: str, name: str = "Unnamed Budget", total = 0):
+    budget_doc = {
+      "user_id": user_id, #Primary key
+      "name": name,       #Primary Key
+      "total_amount": total, 
+      "created_on": datetime.utcnow()
+    }
+
+  @staticmethod
+  def update_budget(user_id: str, update_data):
+    return db.budgets.update_one({"user_id": user_id}, {"$set": update_data})
+  
+  @staticmethod
+  def add_collection_budget(user_id: str, category_name: str):
+    """
+    Docstring for add_collection_budget
+    
+    :param user_id: String Version of User Object ID
+    :type user_id: str
+    :param category_name: String of Added Category
+    :type category_name: str
+    """
+    return db.budgets.update_one({"user_id": user_id}, {"$addToSet": {"categories": category_name}}, upsert=False)
+  
+  @staticmethod
+  def rename_collection_in_budget(user_id: str, category_name: str, new_name: str):
+    """
+    Docstring for rename_collection_in_budget
+    
+    :param user_id: Description
+    :type user_id: str
+    :param category_name: Description
+    :type category_name: str
+    :param new_name: Description
+    :type new_name: str
+    """
+    return db.budgets.update_one({"user_id": user_id, "categories": category_name}, {"$set": {"categories.$": new_name}}, upsert=False)
+  
+  @staticmethod
+  def remove_collection_budget(user_id: str, category_name: str):
+    """
+    Docstring for remove_collection_budget
+    
+    :param user_id: Description
+    :type user_id: str
+    :param category_name: Description
+    :type category_name: str
+    """
+    return db.budgets.update_one({"user_id": user_id}, {"$pull": {"categories": category_name}}, upsert=False)

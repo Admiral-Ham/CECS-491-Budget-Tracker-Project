3817922810b05d6e22309329da1d9a87cd9c19bd
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2025-12-08 16:40:43 -0800

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -1,14 +1,32 @@
-# services/user_service.py
+from data.db import db
+from  user_services.user_managements import create_user, get_all_users, validate_user, unique_email
 
-from data.user_model import UserModel
+def add_user_service(data: dict):
+    """
+    Adds user data.
 
-class UserService:
+    Inputs: data = dictionary
 
-    @staticmethod
-    def get_user(email: str):
-        user = UserModel.get_user_by_email(email)
-        if not user:
-            return {"success": False, "message": "User not found"}
-
+    Returns:
+            User_id = int
+    """
+    valid_user = validate_user(data)
+    if not valid_user.get("success"): #if success = False, triggers if statement and returns an error message
+        return valid_user.get("message")
+    
+    if not unique_email(valid_user.get("return")):
+        return {"error": "Email not Unique"}
+    else:
+        create_user(valid_user.get("return"))
+        
+    #if "email" not in data or "name" not in data:
+        #return {"error": "Missing required fields"}
+    
+def list_users_service():
+    """
+    Return a list of all users 
+    """
+    users = get_all_users()
+    for user in users:
         user["_id"] = str(user["_id"])
-        return {"success": True, "user": user}
+    return users

482727ec1cdde7476e60027cf4934d53fb25657b
Admiral-Ham <ai.does.math152@gmail.com>
2026-02-25 16:37:56 -0800

    updated user settings and created unique identifiers

--- schemas/user_document.py
+++ schemas/user_document.py
@@ -1,15 +1,26 @@
-from typing import Optional
-
-import pymongo
+from pymongo import IndexModel, ASCENDING, DESCENDING
 from pydantic import BaseModel, EmailStr
 from beanie import  Document, Indexed
 from datetime import datetime
 
 class User(Document):
+    # Name and email need to be unique
     name: str
     email: EmailStr
     password_hash: str
     creation_time: datetime
 
     class Settings:
-        
+        name = "users"
+        indexes = [
+            IndexModel(
+                ("name", ASCENDING),
+                name = "unique_name",
+                unique = True
+            ),
+            IndexModel(
+                ("email", ASCENDING),
+                name = "unique_email",
+                unique = True
+            )
+        ]

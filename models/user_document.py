dcc5f12d05c93b9566ea5fd35c7572d3f707c1f6
Admiral-Ham <ai.does.math152@gmail.com>
2026-03-17 21:28:27 -0700

    Added Class projection to User and changed datetime default

--- models/user_document.py
+++ models/user_document.py
@@ -1,19 +1,23 @@
 from pymongo import IndexModel, ASCENDING
-from pydantic import EmailStr, Field
+from pydantic import EmailStr, Field, BaseModel
 from beanie import  Document
-from datetime import datetime
+from datetime import datetime, UTC
 
 class User(Document):
     # Name and email need to be unique
     name:           str
     email:          EmailStr
     password_hash:  str
-    creation_time:  datetime = Field(default_factory=datetime.utcnow)
+    creation_time:  datetime = Field(default_factory=datetime.now(UTC))
 
     model_config = {
         "populate_by_name": True,
         "extra": "forbid"
     }
+    class UserProjection(BaseModel):
+        name: str
+        email: EmailStr
+        creation_time: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
         name = "users"

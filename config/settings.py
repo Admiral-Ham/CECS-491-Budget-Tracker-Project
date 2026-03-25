a85f92e897f6fa4b3df3024f1c3fcd9567f2599d
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2026-03-25 14:20:20 -0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,6 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "budgettracker")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))

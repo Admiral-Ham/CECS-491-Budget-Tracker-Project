4e40e61ccc83ed07c2c0a7a51945f45e5da96be2
boomclap156 <156jasonle@gmail.com>
2026-04-13 12:38:27 -0700

    Update transaction_routes.py

--- routes/transaction_routes.py
+++ routes/transaction_routes.py
@@ -3,11 +3,27 @@ from beanie import PydanticObjectId
 from dns import transaction
 from transaction_document import Transaction
 from fastapi import FastAPI, APIRouter
+from models.user_document import User
+from models.budget_document import Budget
+
 
 router = APIRouter()
 
 @router.post("/transactions", response_model=Transaction)
 async def create_transaction(transaction: Transaction):
+    user = await User.get(transaction.user_id.ref.id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    #validate budget to user
+    if transaction.budget_id is not None:
+        budget = await Budget.get(transaction.budget_id.ref.id)
+        if budget is None:
+            raise HTTPException(status_code=404, detail="Budget not found")
+        if budget.user_id.ref.id != transaction.user_id.ref.id:
+            raise HTTPException(status_code=403, detail="Budget does not belong to this user")
+    
+    await transaction.insert()
     return transaction
 
 @router.get("{user_id}/transactions")

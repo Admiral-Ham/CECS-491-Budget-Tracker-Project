dccda1e181057b9eb8a46b7fbebca080e510bbfb
Ionics <142382277+Ionecs-1@users.noreply.github.com>
2026-04-16 11:23:52 -0700

    Update budget_document.py
    functional budget_document 
    "model_rebuild()" updated version method

--- models/budget_document.py
+++ models/budget_document.py
@@ -1,15 +1,17 @@
 from datetime import datetime, UTC
-from beanie import  Document, Link, BackLink
+from beanie import Document, Link, BackLink
 from pydantic import Field, BaseModel
 from pymongo import IndexModel, ASCENDING
 
 from models.user_document import User
 
+
 def utc_now():
     return datetime.now(UTC)
 
+
 class Budget(Document):
-    user_id: Link[User] # Store user_id
+    user_id: Link[User]
     categories: BackLink["Category"] = Field(original_field="budget_id")
     name: str
     creation_time: datetime = Field(default_factory=utc_now)
@@ -24,24 +26,23 @@ class Budget(Document):
         name: str
         creation_time: datetime
 
-    """One user cannot have duplicate budget names and different users can both have same budget names"""
     class Settings:
         name = "budgets"
         indexes = [
             IndexModel(
-            [("user_id.$id", ASCENDING)], #[("name", 1)],
+                [("user_id.$id", ASCENDING)],
                 name="user_id_index",
             ),
             IndexModel(
-            [("creation_time", ASCENDING)],
-                name ="creation_time_index",
+                [("creation_time", ASCENDING)],
+                name="creation_time_index",
             ),
             IndexModel(
-            [("user_id.$id", ASCENDING), ("name", ASCENDING)],
-                name ="unique_budget_name_per_user",
+                [("user_id.$id", ASCENDING), ("name", ASCENDING)],
+                name="unique_budget_name_per_user",
                 unique=True,
             ),
         ]
 
-from category_document import Category
-Budget.update_forward_refs()
+from models.category_document import Category
+Budget.model_rebuild()
